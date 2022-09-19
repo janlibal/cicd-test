e8f9a87ecd590ab5ace13aa7c0e16ea8da97dc57
unknown <knoflik21@seznam.cz>
2022-09-19 12:24:46 +0200

    overhaul

--- dist/server.js
+++ dist/server.js
@@ -3,6 +3,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = __importDefault(require("./app"));
-const PORT = 8080;
-app_1.default.listen(PORT, () => console.log(`running on port ${PORT}`));
+//import app from "./app"
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+//const PORT: Number = 8080
+const port = 8080;
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'Hello world',
+    });
+});
+//app.listen(PORT, (): void => console.log(`running on port ${PORT}`))
+app.listen(port, () => {
+    console.log(`Server started on port: ${port}`);
+});
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,22 @@
-import app from "./app"
+//import app from "./app"
+import express, {Request, Response, NextFunction} from 'express'
 
-const PORT: Number = 8080
+const app = express()
 
-app.listen(PORT, (): void => console.log(`running on port ${PORT}`))
+
+
+//const PORT: Number = 8080
+const port = 8080
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'Hello world',
+    })
+}) 
+
+//app.listen(PORT, (): void => console.log(`running on port ${PORT}`))
+app.listen(port, () => {
+    console.log(`Server started on port: ${port}`)
+})
+
